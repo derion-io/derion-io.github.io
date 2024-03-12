26c775dca201f24f68258954bb073cf3c4074661
Zergity <zergity@gmail.com>
2024-03-12 19:19:51 +0700

    postions fix and maturity tooltips

---
+++ docs/static/js/302.c1bc9253.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkderivable_interface=self.webpackChunkderivable_interface||[]).push([[302],{30302:(e,a,c)=>{c.r(a),c.d(a,{default:()=>r});const r={}}}]);
+//# sourceMappingURL=302.c1bc9253.chunk.js.map
