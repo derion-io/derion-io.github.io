296f8eb42bb25bc335b258946f632db9bb265cda
Zergity <zergity@gmail.com>
2026-09-20 13:44:55 +0700

    deploy interface @ b959f20

--- docs/assets/index-C_rgZiyZ.js
+++ docs/assets/index-C_rgZiyZ.js
@@ -1,4 +1,4 @@
-import{M as rt,T as Oe,R as j,C as P,a as me,O as K,b as ne,E as q,c as mo}from"./index-BbUQ_iGg.js";import{h as fo}from"./index-zUtnC9u0.js";/**
+import{M as rt,T as Oe,R as j,C as P,a as me,O as K,b as ne,E as q,c as mo}from"./index-BSlLlgHj.js";import{h as fo}from"./index-_s-eqs-X.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
