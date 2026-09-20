b2e05188e9ebb778b75f6f73c4b258b67350670f
Zergity <zergity@gmail.com>
2026-09-20 12:25:45 +0700

    deploy interface @ 26798f8

--- docs/assets/index-DT62X8fN.js
+++ docs/assets/index-DT62X8fN.js
@@ -1,4 +1,4 @@
-import{M as rt,T as Oe,R as j,C as P,a as me,O as K,b as ne,E as q,c as mo}from"./index-B_cpZ95t.js";import{h as fo}from"./index-CuH6DW_4.js";/**
+import{M as rt,T as Oe,R as j,C as P,a as me,O as K,b as ne,E as q,c as mo}from"./index-BbUQ_iGg.js";import{h as fo}from"./index-zUtnC9u0.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
